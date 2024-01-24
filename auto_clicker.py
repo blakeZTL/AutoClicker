d0c0945298400273b7eee2c911afcf3652418adb
Blake <james.b.bradford@faa.gov>
2024-01-24 18:54:21 -0500

    Basic formatting

--- auto_clicker.py
+++ auto_clicker.py
@@ -6,14 +6,12 @@ import time
 
 running = False
 
-
 def auto_clicker(interval):
     global running
     running = True
     while running:
         pyautogui.click()
         time.sleep(interval)
-
 
 def on_toggle_key():
     """
@@ -34,7 +32,6 @@ def on_toggle_key():
     else:
         interval = float(entry_interval.get())
         Thread(target=auto_clicker, args=(interval,)).start()
-
 
 # GUI setup
 root = tk.Tk()
